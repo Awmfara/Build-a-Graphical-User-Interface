4c590d2889ce847f72804d9b0252be0bdaa6b575
Awmfara <69292567+Awmfara@users.noreply.github.com>
2020-09-17 20:54:40 +1000

    More functionality

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;// allows for functions in Unity Editor
+using UnityEngine.SceneManagement;//allows loading of scenes
+
+public class MainMenu : MonoBehaviour
+{
+    #region Variables
+    [Header("Variables")]
+    [SerializeField, Tooltip("Type in Name of Opening Game Scene")]
+    public string LoadScene = "GameScene";
+
+    #endregion
+
+    /// <summary>
+    /// Loads the opening Scene of the Game
+    /// </summary>
+    public void StartGame()
+    {
+        SceneManager.LoadScene(LoadScene);
+    }
+    /// <summary>
+    /// Quits Application. If in Unity Editor, exits PlayMode
+    /// </summary>
+    public void QuitGame()
+    {
+#if UNITY_EDITOR // if In Unity Editor
+        EditorApplication.ExitPlaymode(); //Exits Playmode
+#endif
+        Application.Quit(); //Quits Application
+    }
+}
